7f1d64dd9f034a94fab2dba6524c3d8e4afb99ec
rgabs <rahulgaba16@gmail.com>
2019-08-23 00:46:13 +0530

    Add basic support for PlainTextEdit

---
+++ src/components/PlainTextEdit/index.ts
@@ -0,0 +1,56 @@
+
+import { registerComponent } from "../config";
+import { QPlainTextEdit, QPlainTextEditEvents } from "@nodegui/nodegui";
+import { ViewProps, setProps as setViewProps } from "../View";
+
+interface PlainTextEditProps extends ViewProps {
+  children?: string;
+  text?: string;
+}
+
+const setProps = (
+  widget: QPlainTextEdit,
+  newProps: PlainTextEditProps,
+  oldProps: PlainTextEditProps
+) => {
+  const setter: PlainTextEditProps = {
+    set text(text: string) {
+      text ? widget.setPlainText(text) : widget.clear();
+    },
+  };
+  Object.assign(setter, newProps);
+  setViewProps(widget, newProps, oldProps);
+};
+
+export const PlainTextEdit = registerComponent<PlainTextEditProps>({
+  id: "plaintextedit",
+  getContext() {
+    return {};
+  },
+  shouldSetTextContent: () => {
+    return true;
+  },
+  createInstance: newProps => {
+    const widget = new QPlainTextEdit();
+    setProps(widget, newProps, {});
+    return widget;
+  },
+  finalizeInitialChildren: () => {
+    return false;
+  },
+  commitMount: (instance, newProps, internalInstanceHandle) => {
+    return;
+  },
+  prepareUpdate: (
+    instance,
+    oldProps,
+    newProps,
+    rootContainerInstance,
+    hostContext
+  ) => {
+    return true;
+  },
+  commitUpdate: (instance, updatePayload, oldProps, newProps, finishedWork) => {
+    setProps(instance as QPlainTextEdit, newProps, oldProps);
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { Text } from "./components/Text";
 export { Button } from "./components/Button";
 export { CheckBox } from "./components/CheckBox";
 export { LineEdit } from "./components/LineEdit";
+export { PlainTextEdit } from "./components/PlainTextEdit";
 export { ProgressBar } from "./components/ProgressBar";
 export { RadioButton } from "./components/RadioButton";
 export { Image } from "./components/Image";
