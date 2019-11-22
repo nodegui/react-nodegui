210102dbfea87b2921cbf6674a74c23b58ba3168
Atul R <atulanand94@gmail.com>
2019-11-23 00:08:26 +0100

    Upgrades nodegui to 0.1.6

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Renderer, Button, Window, View } from "./index";
+
 const App = () => {
   return (
     <Window>
-      <View>
+      <View style={containerStyle}>
         <Button style={buttonStyle} text={"Hello"} />
         <Button style={buttonStyle} text={"World"} />
       </View>
     </Window>
   );
 };
+
+const containerStyle = `
+  flex: 1; 
+  justify-content:'center'; 
+  border: 1px solid blue;
+  padding: 10;
+`;
 const buttonStyle = `
   color: white;
 `;
+
 Renderer.render(<App />);
