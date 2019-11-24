c77463536c807a86524225957be71ffe265dcd3e
Atul R <atulanand94@gmail.com>
2019-11-25 00:06:13 +0100

    Deploy website version based on 12386093b3b8d72a5e14d1f33cbf3489f0859233

--- metadata---docs-api-globalsb-8-a-2be.2f96c6e83129ef21b7fa.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{46:function(i){i.exports=JSON.parse('{"id":"api/globals","title":"@nodegui/react-nodegui","sidebar_label":"Globals","description":"## Index","source":"@site/docs/api/globals.md","permalink":"/docs/api/globals","sidebar":"api","category":"Others","previous":"api/interfaces/windowprops","previous_title":"WindowProps"}')}}]);
