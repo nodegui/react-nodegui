ae3a9cdf9b21032be4fbef7d9a161d4228af8789
Serhii Matrunchyk <serhii@digitalidea.studio>
2020-02-12 01:45:07 +0200

    Add more properties from an abstract class

--- src/components/Slider/RNSlider.ts
+++ src/components/Slider/RNSlider.ts
@@ -7,6 +7,16 @@ export interface SliderProps extends ViewProps<QSliderSignals> {
   tickInterval?: number;
   tickPosition?: TickPosition;
   orientation?: Orientation;
+  minimum?: number;
+  maximum?: number;
+  invertedAppearance?: boolean;
+  invertedControls?: boolean;
+  pageStep?: number;
+  singleStep?: number;
+  isSliderDown?: boolean;
+  sliderPosition?: number;
+  hasTracking?: boolean;
+  value?: number;
 }
 
 const setSliderProps = (
@@ -21,8 +31,38 @@ const setSliderProps = (
     set tickPosition(tickPosition: TickPosition) {
       widget.setTickPosition(tickPosition);
     },
+    set invertedAppearance(inverted: boolean) {
+      widget.setInvertedAppearance(inverted);
+    },
+    set invertedControls(inverted: boolean) {
+      widget.setInvertedControls(inverted);
+    },
+    set maximum(maximum: number) {
+      widget.setMaximum(maximum);
+    },
+    set minimum(minimum: number) {
+      widget.setMinimum(minimum);
+    },
     set orientation(orientation: Orientation) {
       widget.setOrientation(orientation);
+    },
+    set pageStep(step: number) {
+      widget.setPageStep(step);
+    },
+    set singleStep(step: number) {
+      widget.setSingleStep(step);
+    },
+    set isSliderDown(down: boolean) {
+      widget.setSliderDown(down);
+    },
+    set sliderPosition(position: number) {
+      widget.setSliderPosition(position);
+    },
+    set hasTracking(enable: boolean) {
+      widget.setTracking(enable);
+    },
+    set value(value: number) {
+      widget.setValue(value);
     },
   };
   Object.assign(setter, newProps);
