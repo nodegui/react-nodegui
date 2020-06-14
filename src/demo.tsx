1f612cccf6390d581d67305e6ebf4b5a815bd769
Chris Shepherd <chris@chrisshepherd.me>
2020-06-14 21:08:50 +0100

    Enable Menu to have Action as children

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,42 +1,59 @@
 import React from "react";
-import { QIcon, QAction, QApplication } from "@nodegui/nodegui";
+import { QIcon, QApplication, QKeySequence } from "@nodegui/nodegui";
 import path from "path";
-import { MenuBar, Menu, SystemTrayIcon, Renderer, Window } from ".";
+import { Action, MenuBar, Menu, SystemTrayIcon, Renderer, Window } from ".";
 
-const quitAction = new QAction();
-quitAction.setText("Quit");
-quitAction.addEventListener("triggered", () => {
-  const app = QApplication.instance();
-  app.exit(0);
-});
-
-const fileActions: QAction[] = [quitAction];
-
-const sayHi = new QAction();
-sayHi.setText("Hello");
-sayHi.addEventListener("triggered", () => {
-  console.log("hello");
-});
-
-const randActions: QAction[] = [sayHi];
+const quitAction = (
+  <Action
+    on={{
+      triggered: () => {
+        QApplication.instance().exit(0);
+      },
+    }}
+    shortcut={new QKeySequence("Ctrl+Q")}
+    text="Quit"
+  />
+);
+const sayHiAction = (
+  <Action
+    on={{
+      triggered: () => {
+        console.log("hello");
+      },
+    }}
+    text="Hello"
+  />
+);
 
 const trayIcon = new QIcon(
   path.join(__dirname, "../extras/assets/nodegui.png")
 );
-const separatorAction = new QAction();
-separatorAction.setSeparator(true);
-
-const systemTrayMenuActions = [sayHi, separatorAction, quitAction];
 
 const App = () => {
   return (
     <Window>
       <SystemTrayIcon icon={trayIcon} tooltip="React Nodegui" visible>
-        <Menu actions={systemTrayMenuActions} />
+        <Menu>
+          <Action
+            on={{
+              triggered: () => {
+                console.log("print");
+              },
+            }}
+            text="Print"
+            shortcut={new QKeySequence("Ctrl+P")}
+          />
+          <Action separator />
+          {quitAction}
+        </Menu>
       </SystemTrayIcon>
       <MenuBar>
-        <Menu title={"File"} actions={fileActions} />
-        <Menu title={"Random"} actions={randActions} />
+        <Menu title="Random">
+          {sayHiAction}
+          <Action separator />
+          {sayHiAction}
+          {quitAction}
+        </Menu>
       </MenuBar>
     </Window>
   );
