037a52847984979d04850648d98c0ffde24209d1
Atul R <atulanand94@gmail.com>
2021-02-27 20:58:22 +0100

    bump nodegui

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { Text, Renderer, Window } from ".";
+import { Button } from "./components/Button";
+import { View } from "./components/View";
 
 const App = () => {
   return (
-    <Window>
-      <Text>Hello</Text>
+    <Window styleSheet={styleSheet}>
+      <View id="container">
+        <View id="textContainer">
+          <Text>Hello</Text>
+        </View>
+        <View>
+          <Button text="Click me"></Button>
+        </View>
+      </View>
     </Window>
   );
 };
+
+const styleSheet = `
+  #container {
+    flex: 1;
+    min-height: '100%';
+    justify-content: 'center';
+  }
+  #textContainer {
+    flex-direction: 'row';
+    justify-content: 'space-around';
+    align-items: 'center';
+  }
+`;
 
 Renderer.render(<App />);
